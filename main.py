a4f9947aa073e237a8163641d04aaac6b93b67a5
Konstanty <konstanty.klosiewicz@gmail.com>
2026-01-16 08:29:44 +0100

    Testing sentiment analysis

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
+from textblob import TextBlob
+import cleantext
+import nltk
 
 def main():
     print("Hello from news-sentiment-gpw!")
-    df = pd.Dataframe({"A":1, "B":2, "C":3, "D":4, "E":5})
-    df.head()
-    df.tail()
+    dic = {"A":1, "B":2, "C":3, "D":4, "E":5}
+    df = pd.DataFrame(dic.items())
+    print(df.head())
+
+def ui():
+    st.header("News Sentiment Analysis")
+    with st.expander("Analyze text"):
+        text = st.text_area("Your text: ")
+        if text:
+            blob = TextBlob(text)
+            st.write('Polarity: ', round(blob.sentiment.polarity, 2))
+            st.write('Subjectivity: ', round(blob.sentiment.subjectivity, 2))
+
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    ui()
